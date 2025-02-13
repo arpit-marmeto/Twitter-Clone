acde44b7e1d64b4bccb3644a518c5beeed623333
arpit-marmeto <arpit@marmeto.com>
2025-02-14 02:03:40 +0530

    ..

--- src/components/tagfeed.jsx
+++ src/components/tagfeed.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import Post from "./Post";
+import Post from "./post";
 
 const TagFeed = () => {
   const { hashtag } = useParams();
