02314eddc38343e83ccb615e728077f1ee32d760
arpit-marmeto <arpit@marmeto.com>
2025-02-14 18:04:16 +0530

    ..

--- src/components/bottomnavigationbar.jsx
+++ src/components/bottomnavigationbar.jsx
@@ -1,7 +1,6 @@
 import { useState, useEffect } from "react";
 import PropTypes from "prop-types";
 import { Home, Search, Bell, Mail, SquarePen } from "lucide-react";
-import CreatePostPopup from "./createpostpopup";
 
 const menuItems = [
   { id: 1, icon: <Home size={24} />, link: "#" },
@@ -11,9 +10,8 @@ const menuItems = [
   { id: 5, icon: <Mail size={24} />, link: "#" },
 ];
 
-const BottomNavigationBar = ({ user, onPostSubmit }) => {
+const BottomNavigationBar = ({ user }) => {
   const [isScrolling, setIsScrolling] = useState(false);
-  const [showCreatePostPopup, setShowCreatePostPopup] = useState(false);
 
   useEffect(() => {
     let scrollTimeout;
@@ -29,33 +27,30 @@ const BottomNavigationBar = ({ user, onPostSubmit }) => {
   }, []);
 
   return (
-    <>
-      <nav
-        className={`fixed bottom-0 left-0 w-full flex justify-around items-center py-2 transition-all duration-300 backdrop-blur-md 
-          ${isScrolling ? "opacity-50" : "opacity-100"} 
-          bg-white dark:bg-[var(--color-dark)] border-t border-gray-300 dark:border-gray-700`}
-      >
-        {menuItems.map((item) => (
-          <button
-            key={item.id}
-            className={`p-3 flex items-center justify-center transition-colors duration-300 
-              ${
-                item.special
-                  ? "bg-blue-500 text-white w-12 h-12 rounded-full hover:bg-blue-600"
-                  : "text-black dark:text-white hover:bg-gray-200 dark:hover:bg-gray-800 p-2 rounded-lg"
-              }`}
-          >
-            {item.icon}
-          </button>
-        ))}
-      </nav>
-    </>
+    <nav
+      className={`fixed bottom-0 left-0 w-full flex justify-around items-center py-2 transition-all duration-300 backdrop-blur-md 
+        ${isScrolling ? "opacity-50" : "opacity-100"} 
+        bg-white dark:bg-[var(--color-dark)] border-t border-gray-300 dark:border-gray-700`}
+    >
+      {menuItems.map((item) => (
+        <button
+          key={item.id}
+          className={`p-3 flex items-center justify-center transition-colors duration-300 
+            ${
+              item.special
+                ? "bg-blue-500 text-white w-12 h-12 rounded-full hover:bg-blue-600"
+                : "text-black dark:text-white hover:bg-gray-200 dark:hover:bg-gray-800 p-2 rounded-lg"
+            }`}
+        >
+          {item.icon}
+        </button>
+      ))}
+    </nav>
   );
 };
 
 BottomNavigationBar.propTypes = {
   user: PropTypes.object.isRequired,
-  onPostSubmit: PropTypes.func.isRequired,
 };
 
 export default BottomNavigationBar;
